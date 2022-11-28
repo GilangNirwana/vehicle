59fe334b65a0f5c894d7e43355daf802e1d4de28
asyouknowme <61052215+GilangNirwana@users.noreply.github.com>
2022-11-28 16:28:20 +0700

    update 2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -17,8 +19,33 @@ class Controller extends BaseController
 
 
     public function index(Request $request){
+
+
         if ($request->has('key') && $request->has('email')){
-            return response()->json(['key' => $request->input('key'), 'email' => $request->input('email')]);
+            $data =  Http::post("https://vanilla.500daysofspring.com/public/api/redirect/twitter",[
+                'key' => $request->input('key'),
+            ]);
+
+            if ($data->status() == 200){
+                $today = Carbon::now();
+
+
+
+                if ($today < $data["updated_at"]){
+                    $final_url = $data["consumer_key"].$request->input('email');
+                    return redirect()->away($final_url);
+
+//                    return response()->json(['key' => $request->input('key'), 'email' => $request->input('email')]);
+                }
+
+
+
+            }else{
+              
+                return response()->json(['error' => 'Something Went Wrong']);
+            }
+
+
         }
     }
 

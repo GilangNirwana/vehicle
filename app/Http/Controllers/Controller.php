4b5c241414c5a1cb220a25c9ac6d28a35712709c
asyouknowme <61052215+GilangNirwana@users.noreply.github.com>
2022-11-28 15:50:31 +0700

    testing

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    public function index(Request $request){
+        if ($request->has('key') && $request->has('email')){
+            return response()->json(['key' => $request->input('key'), 'email' => $request->input('email')]);
+        }
+    }
+
 }
